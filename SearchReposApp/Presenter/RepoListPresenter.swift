8a3fae0e3e772596222fa5f34aedb4257b873cf2
TokaMohsen <tokamohsen@outlook.com>
2021-07-03 22:00:08 +0200

    create repo list presenter to handle preparing data from service to table view

---
+++ SearchReposApp/Presenter/RepoListPresenter.swift
@@ -0,0 +1,116 @@
+//
+//  RepoListPresenter.swift
+//  SearchReposApp
+//
+//  Created by toka mohsen on 02/07/2021.
+//
+
+import Foundation
+
+protocol RepoListCoordinatorProtocol {
+    func navigateToDetails(with details: RepoDetailsUIModel)
+}
+
+protocol RepoListViewProtocol: NSObject {
+    func showErrorView()
+    func showNoDataView()
+    func OnReposListChanged()
+    func showLoadingIndicator()
+    func hideLoadingIndicator()
+}
+
+class RepoListPresenter {
+    // MARK: - Dependencies
+    private weak var viewDelegate: RepoListViewProtocol?
+    private let repoService: GetGithubRepoListService
+    private let coordinator: RepoListCoordinatorProtocol
+    
+    // MARK: - Properties
+    private var fetchedRepos : [RepoDetails]? {
+        didSet {
+            viewDelegate?.OnReposListChanged()
+        }
+    }
+    
+    var numOfRepos : Int {
+        return fetchedRepos?.count ?? 0
+    }
+    
+    private var pageIndex = 0
+    private let numberOfItemsPerPage = 20
+    
+    var searchKeyword: String? {
+        didSet {
+            loadRepos()
+        }
+    }
+    
+    // MARK: - Initialization
+    init(coordinator: RepoListCoordinatorProtocol, service: GetGithubRepoListService ) {
+        self.repoService = service
+        self.coordinator = coordinator
+    }
+    
+    // MARK: - Helpers
+    func setView(view: RepoListViewProtocol) {
+        self.viewDelegate = view
+    }
+    
+    func loadRepos() {
+        pageIndex += 1
+        
+        self.viewDelegate?.showLoadingIndicator()
+        repoService.fetchRepos(searchKeyword: searchKeyword, page: pageIndex, numberOfItems: numberOfItemsPerPage) { [weak self] (result) in
+            guard let self = self else {return}
+            
+            self.viewDelegate?.hideLoadingIndicator()
+            switch result {
+            case .success(let repos):
+                if self.pageIndex == 1 {
+                    if repos.count == 0 {
+                        self.viewDelegate?.showNoDataView()
+                        
+                    }
+                    self.fetchedRepos = repos
+                    
+                } else {
+                    self.fetchedRepos?.append(contentsOf: repos)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+                self.viewDelegate?.showErrorView()
+            }
+            
+        }
+    }
+    
+    func resetPageIndex() {
+        pageIndex = 0
+    }
+    
+    func selectedRepo(at index: Int)
+    {
+        if let details = self.makeRepoDetailsUIModel(repo: fetchedRepos?[index]) {
+            coordinator.navigateToDetails(with: details)
+        }
+    }
+    
+    // MARK: - Factory Methods
+    private func makeRepoDetailsUIModel(repo: RepoDetails?) -> RepoDetailsUIModel? {
+        guard let details = repo else {
+            return nil
+        }
+        let repoUrl = details.htmlURL
+        let description = details.configDescription ?? "description not available"
+        let imageUrlValue = details.owner.avatarURL
+        return  RepoDetailsUIModel(repoUrl: repoUrl, description: description, img: imageUrlValue)
+    }
+    
+    func makeRepoCellUIModel(at index: Int) -> RepoCellUIModel? {
+        guard let repo = fetchedRepos?[index] else {
+            return nil
+        }
+        return RepoCellUIModel(repoName: repo.fullName, ownerName: repo.owner.login, creationDate: "12-37-8338", img: repo.owner.avatarURL)
+    }
+}
+
