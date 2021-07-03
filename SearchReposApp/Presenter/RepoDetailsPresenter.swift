c0559bf54107f651f4b6b20e79d4f28f391cb1d4
TokaMohsen <tokamohsen@outlook.com>
2021-07-03 21:58:44 +0200

    create repoDetails presenter to handle ui model

---
+++ SearchReposApp/Presenter/RepoDetailsPresenter.swift
@@ -0,0 +1,29 @@
+//
+//  RepoDetailsPresenter.swift
+//  SearchReposApp
+//
+//  Created by toka mohsen on 03/07/2021.
+//
+
+import Foundation
+
+class RepoDetailsPresenter {
+    private let repoDetailsModel : RepoDetailsUIModel
+    
+    init(repoDetailsModel: RepoDetailsUIModel ) {
+        self.repoDetailsModel = repoDetailsModel
+    }
+    
+    var description: String?
+    {
+        return repoDetailsModel.description
+    }
+    var repoUrl: String?
+    {
+        return repoDetailsModel.repoUrl
+    }
+    var img: URL?
+    {
+        return URL(string: repoDetailsModel.description)
+    }
+}
