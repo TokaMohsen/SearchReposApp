10e05c1d54d379aee81ec587394173f2bb98e2a6
TokaMohsen <tokamohsen@outlook.com>
2021-07-03 22:04:41 +0200

    create repo details ui model

---
+++ SearchReposApp/Model/Local/RepoDetailsUIModel.swift
@@ -0,0 +1,15 @@
+//
+//  RepoDetailsUIModel.swift
+//  SearchReposApp
+//
+//  Created by toka mohsen on 02/07/2021.
+//
+
+import Foundation
+import UIKit
+
+struct RepoDetailsUIModel {
+    let repoUrl: String
+    let description: String
+    let img: String?
+}
