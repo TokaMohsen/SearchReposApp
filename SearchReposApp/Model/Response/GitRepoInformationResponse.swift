d3977a2536dda358146dcbe6390a4a8408cc407e
TokaMohsen <tokamohsen@outlook.com>
2021-07-03 23:04:03 +0200

    update owner info response model

--- SearchReposApp/Model/Response/GitRepoInformationResponse.swift
+++ SearchReposApp/Model/Response/GitRepoInformationResponse.swift
@@ -17,7 +17,7 @@ struct RepoInfo: Codable {
     let configDescription: String
     let fork: Bool
     let url: String
-    let createdAt : Date
+    let createdAt : String
   
 
     enum CodingKeys: String, CodingKey {
@@ -32,33 +32,5 @@ struct RepoInfo: Codable {
         case fork, url
         case createdAt = "created_at"
        
-    }
-}
-
-
-// MARK: - Encode/decode helpers
-
-class JSONNull: Codable, Hashable {
-
-    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
-        return true
-    }
-
-    public var hashValue: Int {
-        return 0
-    }
-
-    public init() {}
-
-    public required init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        if !container.decodeNil() {
-            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
-        }
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encodeNil()
     }
 }
